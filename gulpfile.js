cf7ff97b1ddd9c3a4776e021450590b1022e968a
vietluannguyen711 <vietluannguyen711@gmail.com>
2025-08-14 09:46:18 +0700

    tich hop sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,40 @@
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
 
-// HTML include
+// HTML
 gulp.task('html', () => {
   return gulp.src(['src/pages/**/*.html'])
-    .pipe(fileInclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
+    .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
     .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream()); // <-- Reload browser
+    .pipe(browserSync.stream());
 });
 
-// Copy assets (CSS, JS, images...)
+// Compile Sass
+gulp.task('sass', () => {
+  return gulp.src('src/assets/scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(browserSync.stream());
+});
+
+// Copy JS, Images
 gulp.task('assets', () => {
-  return gulp.src('src/assets/**/*')
-    .pipe(gulp.dest('dist/assets'))
-    .pipe(browserSync.stream()); // <-- Reload browser
+  return gulp.src('src/assets/js/**/*')
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(browserSync.stream());
 });
 
-// Serve project with BrowserSync
+// Serve
 gulp.task('serve', () => {
-  browserSync.init({
-    server: {
-      baseDir: 'dist'
-    },
-    port: 3000
-  });
+  browserSync.init({ server: { baseDir: 'dist' }, port: 3000 });
 
   gulp.watch('src/pages/**/*.html', gulp.series('html'));
   gulp.watch('src/partials/**/*.html', gulp.series('html'));
-  gulp.watch('src/assets/**/*', gulp.series('assets'));
+  gulp.watch('src/assets/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('src/assets/js/**/*.js', gulp.series('assets'));
 });
 
-// Default task
-gulp.task('default', gulp.series('html', 'assets', 'serve'));
+// Default
+gulp.task('default', gulp.series('html', 'sass', 'assets', 'serve'));
